0851c35f769e846e529d948adfb19a168f034dc7
razamirza456 <101307688+razamirza456@users.noreply.github.com>
2022-03-10 08:57:37 +0500

    first assignment completed

---
+++ main.dart
@@ -0,0 +1,30 @@
+
+main(){
+  
+    
+    double tempFarenheit = 86;
+    double C = (86 - 32) / 1.8 ;
+    print('c');
+    String user ='razajaved';
+
+    String FiestName='raza';
+
+    String lastName = 'javed';
+
+    // upper case and lower case
+    print(user.toUpperCase());
+
+    print(user.toLowerCase());
+
+    //isEmpty use for true false
+    print(user.isEmpty);
+
+    print(user.isNotEmpty);
+
+    //find the length of string
+    // 
+    print(user.length);
+
+    print(user.codeUnits);
+  }
+  
